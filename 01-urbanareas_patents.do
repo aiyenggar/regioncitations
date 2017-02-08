969af5cc0ea4ce6a613c564ca55f83631972633b
Ashwin <ashwin.iyenggar15@iimb.ernet.in>
2017-02-08 22:23:08 +0530

    on the way to getting the all urban centres thing going

---
+++ 01-urbanareas_patents.do
@@ -0,0 +1,102 @@
+cap log close
+log using knowledge-flows.log, append
+set more off
+local destdir ~/datafiles/patents/
+
+use `destdir'rawinventor_urban_areas.dta, clear
+
+sort region
+egen iregion = group(region)
+sort iregion year
+bysort iregion year: gen dup_patent_count=_N
+// 13,734,673 observations
+
+sort patent_id iregion
+bysort patent_id iregion: gen patent_region_index=_n
+/* Keep only entry per patent region (this may not be so because of multiple inventors from the same location on the same patent) */
+keep if patent_region_index == 1
+drop inventor_id patent_region_inde
+
+// Add any patent level information here
+merge m:1 patent_id using `destdir'nber.dta, keep(match master) nogen
+sort iregion year
+// We have 8,198,219 observations now
+
+levels category_id, local(catid)
+foreach icat of local catid {
+	bysort iregion year: gen runsumcat`icat'=sum(1)  if category_id==`icat'
+	replace runsumcat`icat'=0 if missing(runsumcat`icat')
+	bysort iregion year: egen cat`icat'=max(runsumcat`icat')
+	drop runsumcat`icat'
+}
+
+levels subcategory_id, local(subcatid)
+foreach l of local subcatid {
+	bysort iregion year: gen runsumsubcat`l'=sum(1)  if subcategory_id==`l'
+	replace runsumsubcat`l'=0 if missing(runsumsubcat`l')
+	bysort iregion year: egen subcat`l'=max(runsumsubcat`l')
+	drop runsumsubcat`l'
+}
+
+drop category_id subcategory_id
+bysort iregion year: gen patent_count=_N
+bysort iregion year: gen region_year_index = _n
+keep if region_year_index == 1
+
+keep year region iregion patent_count dup_patent_count cat* subcat* country_loc country_rawloc
+order year region iregion patent_count dup_patent_count cat* subcat* country*
+drop if missing(region)
+save `destdir'urbanareas.year.patents.nber.dta, replace
+// We have 57,456 observations now
+
+use `destdir'urbanareas.year.patents.nber.dta, clear
+sort iregion year
+egen rate12 = mean(patent_count) if (year > 2000 & year <= 2012), by(iregion)
+bysort iregion: gen pool_dup_patent_count=sum(dup_patent_count)
+replace pool_dup_patent_count = pool_dup_patent_count - dup_patent_count
+bysort iregion: gen pool_patent_count=sum(patent_count)
+replace pool_patent_count = pool_patent_count - patent_count
+
+sort year
+egen yrank = rank(-patent_count), by(year)
+egen poolyrank = rank(-pool_patent_count), by(year)
+egen patent_count_year = sum(patent_count), by(year)
+
+sort year yrank
+bysort year: gen running_patent_count = sum(patent_count)
+gen running_ratio = running_patent_count / patent_count_year
+
+sort region year pool_patent_count
+keep region year patent_count pool_patent_count cat* subcat*
+order region year patent_count pool_patent_count
+
+foreach var of varlist cat* subcat* {
+  gen d`var' = 1 if `var' > 0
+  replace d`var' = 0 if missing(d`var')
+}
+export delimited using `destdir'urbanareas.year.csv, replace
+
+log close
+
+
+
+
+
+
+
+
+
+// br if (!missing(rate12) & rate12 >= 200.0)
+/*
+
+tsset iregion year, yearly
+gen mean12 = L11.patent_count + L10.patent_count + L9.patent_count + L8.patent_count + L7.patent_count + L6.patent_count + L5.patent_count + L4.patent_count + L3.patent_count + L2.patent_count + L1.patent_count + patent_count
+replace mean12=mean12/12
+
+
+gen sum5 = L5.patent_region_count + L4.patent_region_count + L3.patent_region_count + L2.patent_region_count + L1.patent_region_count
+gen rate3 = (patent_region_count*100)/sum3
+egen yrank3 = rank(-rate3), by(year)
+gen rate5 = (patent_region_count*100)/sum5
+egen yrank5 = rank(-rate5), by(year)
+*/
---
+++ 04-urbanareas_integration.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 05:48:51 2017
+
+@author: aiyenggar
+"""
+
+import csv
+
+pheader=list(["region","year","patent_count","pool_patent_count","cat1","cat2","cat3","cat4","cat5","cat6","cat7","subcat11","subcat12","subcat13","subcat14","subcat15","subcat19","subcat21","subcat22","subcat23","subcat24","subcat25","subcat31","subcat32","subcat33","subcat39","subcat41","subcat42","subcat43","subcat44","subcat45","subcat46","subcat49","subcat51","subcat52","subcat53","subcat54","subcat55","subcat59","subcat61","subcat62","subcat63","subcat64","subcat65","subcat66","subcat67","subcat68","subcat69","subcat70","dcat1","dcat2","dcat3","dcat4","dcat5","dcat6","dcat7","dsubcat11","dsubcat12","dsubcat13","dsubcat14","dsubcat15","dsubcat19","dsubcat21","dsubcat22","dsubcat23","dsubcat24","dsubcat25","dsubcat31","dsubcat32","dsubcat33","dsubcat39","dsubcat41","dsubcat42","dsubcat43","dsubcat44","dsubcat45","dsubcat46","dsubcat49","dsubcat51","dsubcat52","dsubcat53","dsubcat54","dsubcat55","dsubcat59","dsubcat61","dsubcat62","dsubcat63","dsubcat64","dsubcat65","dsubcat66","dsubcat67","dsubcat68","dsubcat69","dsubcat70"])
+patentsFile="/Users/aiyenggar/datafiles/patents/urbanareas.year.csv"
+
+forwardmapheader=["fc_year", "fc_region", "fc_pop", "fc_areakm", "fc_total", "fc_sla", "fc_slap", "fc_slpa", "fc_slpap", "fc_sother", "fc_sl", "fc_sa"]
+fmapFile="/Users/aiyenggar/datafiles/patents/forwardmap.csv"
+
+backwardmapheader=["bc_year", "bc_region", "bc_pop", "bc_areakm", "bc_total", "bc_sla", "bc_slap", "bc_slpa", "bc_slpap", "bc_sother", "bc_sl", "bc_sa"]
+bmapFile="/Users/aiyenggar/datafiles/patents/backwardmap.csv"
+
+outputheader=["year", "region", "patents", "pool", "cit_made_total", "cit_made_localinternal", "cit_made_localexternal", "cit_made_nonlocalinternal", "cit_made_nonlocalexternal", "cit_made_other", "cit_made_local", "cit_made_internal", "cit_recd_total", "cit_recd_local", "cit_recd_nonlocal", "cit_recd_other",
+"cit_made_pop", "cit_made_areakm", "cit_recd_pop", "cit_recd_areakm"]
+outputFile="/Users/aiyenggar/datafiles/patents/citations.urbanareas.year.csv"
+
+pDict = dict({})
+patentsf = open(patentsFile, 'r', encoding='utf-8')
+patentsr = csv.reader(patentsf)
+
+for patentsl in patentsr:
+    if patentsr.line_num == 1:
+        continue
+    l = []
+    l.append(patentsl[1]) #year
+    l.append(patentsl[0]) #region
+    pkey = tuple(l)
+    if pkey not in pDict:
+        pDict[pkey] = patentsl #saving the whole line
+    else:
+        print("Duplicate in patentsFile " + str(pkey))
+print("finished reading in patentsFile")
+
+fDict = dict({})
+forwardf = open(fmapFile, 'r', encoding='utf-8')
+forwardr = csv.reader(forwardf)
+for forwardl in forwardr:
+    if forwardr.line_num == 1:
+        continue
+    if not forwardl[0].isdigit():
+        print("In Forward Citations, Skipping " + str(forwardl))
+        continue # skip header lines
+    l = []
+    l.append(forwardl[0])
+    l.append(forwardl[1])
+    fkey = tuple(l)
+    
+    if fkey not in fDict:
+        fDict[fkey] = [int(forwardl[2]), int(forwardl[3]), int(forwardl[4]), int(forwardl[5]), int(forwardl[6]), int(forwardl[7]), int(forwardl[8]), 
+int(forwardl[9]), int(forwardl[10]), int(forwardl[11])]
+    else:
+        # multiple entries due to the division in 03-urbanareas_citations.py
+        prev = fDict[fkey]
+        total = prev[2] + int(forwardl[4])
+        sla = prev[3] + int(forwardl[5])
+        slap = prev[4] + int(forwardl[6])
+        slpa = prev[5] + int(forwardl[7])
+        slpap = prev[6] + int(forwardl[8])
+        sother = prev[7] + int(forwardl[9])
+        sl = prev[8] + int(forwardl[10])
+        sa = prev[9] + int(forwardl[11])
+        fDict[fkey] = [prev[0], prev[1], total, sla, slap, slpa, slpap, sother, sl, sa]
+        
+
+
+bDict = dict({})
+backwardf = open(bmapFile, 'r', encoding='utf-8')
+backwardr = csv.reader(backwardf)
+for backwardl in backwardr:
+    if backwardr.line_num == 1:
+        continue
+    if not backwardl[0].isdigit():
+        print("In Backward Citations, Skipping " + str(backwardl))
+        continue # skip header lines
+    l = []
+    l.append(backwardl[0])
+    l.append(backwardl[1])
+    bkey = tuple(l)
+    
+    if bkey not in bDict:
+        bDict[bkey] = [int(backwardl[2]), int(backwardl[3]), int(backwardl[4]), int(backwardl[5]), int(backwardl[6]), int(backwardl[7]), int(backwardl[8]), int(backwardl[9]), int(backwardl[10]), int(backwardl[11])]
+    else:
+        # multiple entries due to the division in 03-regioncitations.py
+        prev = bDict[bkey]
+        total = prev[2] + int(backwardl[4])
+        sla = prev[3] + int(backwardl[5])
+        slap = prev[4] + int(backwardl[6])
+        slpa = prev[5] + int(backwardl[7])
+        slpap = prev[6] + int(backwardl[8])
+        sother = prev[7] + int(backwardl[9])
+        sl = prev[8] + int(backwardl[10])
+        sa = prev[9] + int(backwardl[11])
+        bDict[bkey] = [prev[0], prev[1], total, sla, slap, slpa, slpap, sother, sl, sa]
+
+outputf = open(outputFile, 'w', encoding='utf-8')
+writer = csv.writer(outputf)
+writer.writerow(outputheader+pheader[4:])
+
+for p in pDict:
+    year = p[0]
+    region = p[1]
+    
+    pv = pDict[p]
+    patents = pv[2]
+    pool = pv[3]
+    dummies = pv[4:]
+    
+    if p in bDict:
+        bv = bDict[p]
+    else:
+        bv = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    cit_made_pop = bv[0]
+    cit_made_areakm = bv[1]
+    cit_made_total = bv[2]
+    cit_made_localinternal = bv[3]
+    cit_made_localexternal = bv[4]
+    cit_made_nonlocalinternal = bv[5]
+    cit_made_nonlocalexternal = bv[6]
+    cit_made_other = bv[7]
+    cit_made_local = bv[8]
+    cit_made_internal = bv[9]
+    
+    if p in fDict:
+        fv = fDict[p]
+    else:
+        fv = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    cit_recd_pop = fv[0]
+    cit_recd_areakm = fv[1]
+    cit_recd_total = fv[2]
+    cit_recd_local = fv[8]
+    cit_recd_nonlocal = int(fv[5]) + int(fv[6])
+    cit_recd_other = fv[7]
+    baserow = [year, region, patents, pool, cit_made_total, cit_made_localinternal, cit_made_localexternal, cit_made_nonlocalinternal, cit_made_nonlocalexternal, cit_made_other, cit_made_local, cit_made_internal, cit_recd_total, cit_recd_local, cit_recd_nonlocal, cit_recd_other, cit_made_pop, cit_made_areakm, cit_recd_pop, cit_recd_areakm]
+    writer.writerow(baserow+dummies)
+
+outputf.close()
