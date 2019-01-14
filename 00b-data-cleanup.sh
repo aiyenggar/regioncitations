3de4c8c6bb2b07e4e94a99bea4f762828187ef58
Ashwin <aiyenggar@users.noreply.github.com>
2019-01-14 23:26:38 +0530

    updated patent.tsv cleanup instructions

--- 00b-data-cleanup.sh
+++ 00b-data-cleanup.sh
@@ -13,11 +13,14 @@ awk -F"\t" '{$1="";$5="";$7="";print}' uspatentcitation.tsv  > processed.uspaten
 
 # I open each of the bad.$file manually and run :1,$s/"//g
 # then concatenate the processed.$file and bad.$file into $file after backing up $file into original folder
-#awk -F"\t" '{$6=""; print}' $DATADIR/patent.tsv > $DATADIR/processed.patent.tsv
-# the bad.patent.tsv file will need post processing (like the one above) before it can be concatenated
+
 # cat processed.location.tsv bad.location.tsv > location.tsv
 # cat processed.rawinventor.tsv bad.rawinventor.tsv > rawinventor.tsv
 # cat processed.rawlocation.tsv bad.rawlocation.tsv > rawlocation.tsv
+# cat processed.patent.tsv bad.patent.tsv > n.patent.tsv
+# the n.patent.tsv file will need post processing (below) after concatenation
+#awk -F"\t" '{$6=""; print}' $DATADIR/n.patent.tsv > $DATADIR/patent.tsv
+# n.patent.tsv which is the cleanedup version of the original can be moved to original
 
 # Important
 # About a 100 lines in location.tsv have matched quotes but they mess up with the lat long. So run :1,$s/"//g on location.tsv
