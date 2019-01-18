d011c3a9d600eca26b80ef73caffe1ba1b358fa6
Ashwin <aiyenggar@users.noreply.github.com>
2019-01-18 21:07:32 +0530

    A faster algorithm to calculate distances in the neighbourhood, but this may still take 12 hours

--- 10-fill-urbanarea.py
+++ 10-fill-urbanarea.py
@@ -21,7 +21,8 @@ latlong_urbanarea = pd.read_csv(file_latlong_urbanarea, usecols = ['latlong', 'l
 latlong_urbanarea['mindist']=round(2 * 3.14159 * 6371,2)
 latlong_urbanarea['near_latlong']=""
 latlong_urbanarea['near_urbanarea']=""
-
+latlong_urbanarea.sort_values(['latitude', 'longitude'], ascending=[True, True])
+treshold = 0.5
 master = latlong_urbanarea.copy(True)
 master.dropna(subset=['urban_area'], inplace=True)
 
@@ -29,6 +30,20 @@ for mindex, masterow in master.iterrows():
     a = masterow['latitude']
     b = masterow['longitude']
     l=(a,b)
+    for nindex, nrow in latlong_urbanarea[(latlong_urbanarea['latitude'] < a + treshold) & (latlong_urbanarea['latitude'] > a - treshold) & (latlong_urbanarea['longitude'] < b + treshold) & (latlong_urbanarea['longitude'] > b - treshold)].iterrows():
+        if (pd.isnull(nrow['urban_area'])):
+            r=(nrow['latitude'], nrow['longitude'])
+            dist = round(geopy.distance.geodesic(l,r).km,2)
+            if (dist < nrow['mindist']):
+                latlong_urbanarea.loc[nindex,'mindist'] = dist
+                latlong_urbanarea.loc[nindex,'near_latlong'] = masterow['latlong']
+                latlong_urbanarea.loc[nindex,'near_urbanarea'] = masterow['urban_area']
+    if (mindex%59 == 0):
+        print(str(mindex))
+        latlong_urbanarea.to_csv(file_filled_urbanarea)  
+latlong_urbanarea.to_csv(file_filled_urbanarea)
+
+'''   
     for findex, fitrow in latlong_urbanarea.iterrows():
         if (pd.isnull(fitrow['urban_area'])):
             if ((fitrow['latitude'] - masterow['latitude']) > 0.4):
@@ -43,3 +58,5 @@ for mindex, masterow in master.iterrows():
         print(str(mindex))
         latlong_urbanarea.to_csv(file_filled_urbanarea)
 latlong_urbanarea.to_csv(file_filled_urbanarea)
+'''
+
