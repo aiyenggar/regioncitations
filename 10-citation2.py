d74e02b9fa076dab5da46b074bb6c76808eff6c7
Ashwin <aiyenggar@users.noreply.github.com>
2019-03-07 10:39:51 +0530

    intermediate version (not tested) in adding the distance calculations

--- 10-citation2.py
+++ 10-citation2.py
@@ -20,15 +20,18 @@ backwardCitationsConfig="Pure-Collapsed"
 fileDatePrefix="20190306"
 urbanareaConfig="ua1"
 calculateCitationDistance=True
+distanceTreshold=30.01
+degreeTreshold=0.3 # to set the bounding box based on latitude and longitude
 outputPrefix = fileDatePrefix + "-" + urbanareaConfig + "-" + "CalcDist-" + str(calculateCitationDistance)
 pathPrefix = "/Users/aiyenggar/processed/patents/"
 
 # the below file needs to be augmented to include a list of latlongid, so whenever uaid is -1, one may fall back onto the latlongid to then calculate the actual distance
 # patent_id,assigneelist,ualist
-keysFile1=pathPrefix + fileDatePrefix + "-" + urbanareaConfig + "-" + "-patent_list_location_assignee.csv"
+keysFile1=pathPrefix + fileDatePrefix + "-" + urbanareaConfig + "-patent_list_location_assignee.csv"
 # application_year,patent_id,citation_id,citation_type,sequence,kind,application_date
 searchFileName=pathPrefix + fileDatePrefix + "-citation.csv"
-
+distancesFile=pathPrefix + "latlong_urbanarea_2.csv"
+latlongFile=pathPrefix + "latlong_urbanarea.csv"
 # Within Cluster, Within Firm: q1
 # Within Cluster, Outside Frim: q2
 # Outside Cluster, Outside Firm: q3
@@ -134,28 +137,58 @@ def adderr(dic, key):
     return dic
 
 # read keysFile1
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Start Reading " + keysFile1)
-inventor_dict = pd.read_csv(keysFile1, usecols = ['patent_id','ualist'], dtype={'patent_id':str,'ualist':str}, index_col='patent_id').to_dict()
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading Inventor Urban Areas")
+inv_uaid_dict = pd.read_csv(keysFile1, usecols = ['patent_id','ualist'], dtype={'patent_id':str,'ualist':str}, index_col='patent_id').to_dict()
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading Assignee")
 assignee_dict = pd.read_csv(keysFile1, usecols = ['patent_id','assigneelist'], dtype={'patent_id':str,'assigneelist':str}, index_col='patent_id').to_dict()
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Done Reading " + keysFile1)
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading Inventor Latlong")
+inv_latlongid_dict = pd.read_csv(keysFile1, usecols = ['patent_id','latlonglist'], dtype={'patent_id':str,'latlonglist':str}, index_col='patent_id').to_dict()
+if calculateCitationDistance:
+    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading Pre-calculated Distances")
+    distances_dict = pd.read_csv(distancesFile, usecols = ['l_latlongid','r_latlongid','distance'], dtype={'l_latlongid':int,'r_latlongid':int,'distance':float}, index_col=['l_latlongid', 'r_latlongid']).to_dict()
+    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading Latlongid to Latitude, Longitude Mapping")
+    latlong_dict = pd.read_csv(latlongFile, usecols = ['latlongid','latitude','longitude'], dtype={'latlongid':int,'latitude':float,'longitude':float}, index_col='latlongid').to_dict()
+    # also read the other two files
+else:
+    inv_latlongid_dict = None
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Done Reading ")
 
 # initialize hashtable
 acc_fwd_cit = {}
 acc_back_cit = {}
 counter = 0
-inventor_error_lines = 0
-assignee_error_lines = 0
+
 inventor_missing_dict = {}
 assignee_missing_dict = {}
 
+"""
+distances_dict['distance'][tuple([47362, 48116])]
+latlong_dict['latitude'][873]
+latlong_dict['longitude'][873]
+"""
+
 # loop through citations
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Processing Citations")
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Processing Citations")
 searchf = open(searchFileName, 'r', encoding='utf-8')
 sreader = csv.reader(searchf)
 t1 = 0
 t2 = 0
 t3 = 0
 status_line = 0
+# raw_citation0 is the value of sreader.line_num at the end
+raw_citation1 = 0 # Number of raw citations (not expanded) that have inventor location problems
+raw_citation2 = 0 # Number of raw citations (not expanded) that have assignee problems
+exp_citation1 = 0 # Number of expanded citations seen
+exp_citation2 = 0 # Number of expanded citations where patent inventor location and citation inventor location are both defined
+exp_citation3 = 0 # Number of expanded citations where patent assignee and citation assignee are both defined
+exp_citation4 = 0 # Number of expanded citations where patent inventor location, citation inventor location, patent assignee and citation assignee are all defined
+exp_citation5 = 0 # Number of expanded citations that qualify for distance calculation intervention
+exp_citation6 = 0 # Number of expanded citations where distance is obtained from the hash table
+exp_citation7 = 0 # Number of expanded citations where distance calculation was forced
+exp_citation8 = 0 # Number of expanded citations where the bounding box avoided the need for distance calculation
+exp_citation9 = 0 # Number of expanded citations where calculating distance assigns citation as local
+exp_citation10 = 0 # Number of expanded citations where calculating distance assigns citation as non-local
+
 for citation in sreader:
     """ Timing """
     start = time.time()
@@ -169,50 +202,53 @@ for citation in sreader:
     kind_citation = citation[5] # B1, A etc
 
     try:
-        tmplist = inventor_dict['ualist'][patent_id]
+        p_latlongid = inv_latlongid_dict['latlongid'][patent_id].split(',')
+        tmplist = inv_uaid_dict['ualist'][patent_id]
         if tmplist != tmplist:
-            inventor_error_lines += 1
+            raw_citation1 += 1
             inventor_missing_dict = adderr(inventor_missing_dict, patent_id)
             p_loc = ['-3']
         else:
             p_loc = tmplist.split(',')
     except KeyError:
-        inventor_error_lines += 1
+        raw_citation1 += 1
         inventor_missing_dict = adderr(inventor_missing_dict, str(sys.exc_info()[1]).split('\'')[1])
         continue # Cannot go on
     except:
         print(str(sys.exc_info()[0]) + " PATENT -> " + str(sys.exc_info()[1]))
-        inventor_error_lines += 1
+        raw_citation1 += 1
         continue # Cannot go on
 
     try:
-        tmplist = inventor_dict['ualist'][citation_id]
+        c_latlongid = inv_latlongid_dict['latlongid'][citation_id].split(',')
+        tmplist = inv_uaid_dict['ualist'][citation_id]
         if tmplist != tmplist:
-            inventor_error_lines += 1
+            raw_citation1 += 1
             inventor_missing_dict = adderr(inventor_missing_dict, citation_id)
             c_loc = ['-3']
         else:
             c_loc = tmplist.split(',')
     except KeyError:
-        inventor_error_lines += 1
+        raw_citation1 += 1
         inventor_missing_dict = adderr(inventor_missing_dict, str(sys.exc_info()[1]).split('\'')[1])
         continue # Cannot go on
     except:
         print(str(sys.exc_info()[0]) + " CITATION -> " + citation_id + " >> " + str(sys.exc_info()[1]))
-        inventor_error_lines += 1
+        raw_citation1 += 1
         continue # Cannot go on
 
+    assignee_error = False
     try:
         tmplist = assignee_dict['assigneelist'][patent_id]
         if tmplist != tmplist:
-            assignee_error_lines += 1
+            assignee_error = True
             assignee_missing_dict = adderr(assignee_missing_dict, patent_id)
             p_ass = ['-3']
         else:
             p_ass = tmplist.split(',')
     except:
         print(str(sys.exc_info()[0]) + " PASS -> " + str(sys.exc_info()[1]))
-        assignee_error_lines += 1
+        assignee_error = True
         assignee_missing_dict = adderr(assignee_missing_dict, str(sys.exc_info()[1]).split('\'')[1])
         p_ass = ['-4']
         # Go on, do not quit
@@ -220,34 +256,83 @@ for citation in sreader:
     try:
         tmplist = assignee_dict['assigneelist'][citation_id]
         if tmplist != tmplist:
-            assignee_error_lines += 1
+            assignee_error = True
             assignee_missing_dict = adderr(assignee_missing_dict, citation_id)
             c_ass = ['-3']
         else:
             c_ass = tmplist.split(',')
     except:
         print(str(sys.exc_info()[0]) + " CASS -> " + citation_id + " >> " + str(sys.exc_info()[1]))
-        assignee_error_lines += 1
+        assignee_error = True
         assignee_missing_dict = adderr(assignee_missing_dict, str(sys.exc_info()[1]).split('\'')[1])
         c_ass = ['-4']
         # Go on, do not quit
-
+    if assignee_error:
+        raw_citation2 += 1
     fc_dict = {}
     bc_dict = {}
 
-    for patloc in p_loc:
-        for citloc in c_loc:
-            for patass in p_ass:
-                for citass in c_ass:
-                    """
-                    if (int(patass) < -1 or int(citass) < -1 or int(patloc) < -1 or int(citloc) < -1):
-                        print(patent_id + " ( " + patass + " ) " +  " : " + " ( " + patloc + " ) -> " + citation_id + " ( " + citass + " ) " + " : " + " ( " + citloc + " )")
-                    """
-                    fc_dict = assign_flow(fc_dict, year, type_citation, int(patloc), int(citloc), int(patass), int(citass))
+    for pind in range(len(p_loc)):
+        for cind in range(len(c_loc)):
+            for apind in range(len(p_ass)):
+                for acind in range(len(c_ass)):
+                    exp_citation1 += 1
+                    patloc = int(p_loc[pind])
+                    citloc = int(c_loc[cind])
+                    patass = int(p_ass[apind])
+                    citass = int(c_ass[acind])
+                    finvloc = False
+                    fass = False
+                    if (patloc >= 0 & citloc >= 0):
+                        exp_citation2 += 1
+                        f1 = True
+
+                    if (patass >= 0 & citass >= 0):
+                        exp_citation3 += 1
+                        f2 = True
+
+                    if (f1 & f2):
+                        exp_citation4 += 1
+
+                    print(patent_id + " ( " + str(patass) + " ) " +  " : " + " ( " + str(patloc) + " ) -> " + citation_id + " ( " + str(citass) + " ) " + " : " + " ( " + str(citloc) + " )")
+
+                    if calculateCitationDistance & patloc >= 0 & citloc < 0: # this is when intervention is possible
+                        exp_citation5 += 1
+                        patllid = int(p_latlongid[pind])
+                        citllid = int(c_latlongid[cind])
+                        dist = sys.maxsize
+                        if tuple([patllid,citllid]) in distances_dict:
+                            exp_citation6 += 1
+                            dist = distances_dict['distance'][tuple([patllid, citllid])]
+                        elif tuple([citllid,patllid]) in distances_dict:
+                            exp_citation6 += 1
+                            dist = distances_dict['distance'][tuple([citllid, patllid])]
+                        else:
+                            l_lat = latlong_dict['latitude'][patllid]
+                            l_long = latlong_dict['longitude'][patllid]
+                            r_lat = latlong_dict['latitude'][citllid]
+                            r_long = latlong_dict['longitude'][citllid]
+                            if (l_lat < r_lat + degreeTreshold) & (l_lat > r_lat - degreeTreshold) & (l_long < r_long + degreeTreshold) & (l_long > r_long - degreeTreshold):
+                                exp_citation7 += 1
+                                dist = haversine(l_lat, l_long, r_lat, r_long)
+                                distances_dict['distance'][tuple([patllid,citllid])] = dist
+                            else:
+                                exp_citation8 += 1
+
+                        if dist < distanceTreshold:
+                            exp_citation9 += 1
+                            assumedcitloc = patloc
+                        else:
+                            exp_citation10 += 1
+                            assumedcitloc = sys.maxsize # something that is different from patloc but not any other loc either
+                        fc_dict = assign_flow(fc_dict, year, type_citation, patloc, assumedcitloc, patass, citass)
+                    else: # no distance calculation
+                        fc_dict = assign_flow(fc_dict, year, type_citation, patloc, citloc, patass, citass)
+
                     if (backwardCitationsConfig == "Expanded"): # count expanded citations received
-                        bc_dict = assign_flow(bc_dict, year, type_citation, int(citloc), int(patloc), int(citass), int(patass))
+                        bc_dict = assign_flow(bc_dict, year, type_citation, citloc, patloc, citass, patass)
                     elif (backwardCitationsConfig == "Pure-Collapsed"): # count pure citations received
-                         bc_dict = assign_flow2(bc_dict, year, type_citation, int(citloc), int(citass), int(patass))
+                         bc_dict = assign_flow2(bc_dict, year, type_citation, citloc, citass, patass)
                     else:
                         print("Undefined backwardCitationsConfig")
                         exit()
@@ -255,20 +340,21 @@ for citation in sreader:
     acc_fwd_cit = update(acc_fwd_cit, fc_dict, [0,0,0,0,0])
     acc_back_cit = update(acc_back_cit, bc_dict, [0,0,0,0,0])
 
-    if sreader.line_num >= status_line + 3500000:
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Processed = " + str(sreader.line_num) + " InvErr = " + str(inventor_error_lines) + " AssErr = " + str(assignee_error_lines) + " t1 = " + str(round(t1,2)))
+    if sreader.line_num >= status_line + 10:
+        print(time.strftime("%Y-%m-%d %H:%M:%S") + " Raw = " + str([sreader.line_num, raw_citation1, raw_citation2]) + " Exp = "  + str([exp_citation1, exp_citation2, exp_citation3, exp_citation4]) + " Dist = " + str([exp_citation5, exp_citation6, exp_citation7, exp_citation8, exp_citation9, exp_citation10]) + " t1 = " + str(round(t1,2)))
         status_line = sreader.line_num
         dump(fc_outputFileName, acc_fwd_cit, fc_outputheader, True)
         dump(bc_outputFileName, acc_back_cit, bc_outputheader, True)
         dump(invErrorFileName, inventor_missing_dict, ["patent_id", "num_lines"], False)
         dump(assErrorFileName, assignee_missing_dict, ["patent_id", "num_lines"], False)
         counter += 1
-        if counter < 0:
+        if counter > 0:
             break
     end = time.time()
     t1 += end - start
 
 # dump final output
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Raw = " + str([sreader.line_num, raw_citation1, raw_citation2]) + " Exp = "  + str([exp_citation1, exp_citation2, exp_citation3, exp_citation4]) + " Dist = " + str([exp_citation5, exp_citation6, exp_citation7, exp_citation8, exp_citation9, exp_citation10]) + " t1 = " + str(round(t1,2)))
 dump(fc_outputFileName, acc_fwd_cit, fc_outputheader, True)
 dump(bc_outputFileName, acc_back_cit, bc_outputheader, True)
 dump(invErrorFileName, inventor_missing_dict, ["patent_id", "num_lines"], False)
