3b90cdffed756f1ef6f8c2216c5e5e2aff4c1881
Ashwin <aiyenggar@users.noreply.github.com>
2020-01-01 10:04:15 +0530

    dual index in pandas

--- 28-experiments.py
+++ 28-experiments.py
@@ -12,12 +12,15 @@ import time
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Beginning Pre-processing")
 df = pd.read_csv(ut.citationFlowsFile, \
                  usecols = ['year', 'uaid', 'patent_id', 'citation_id', 'q0', 'q1', 'q2', 'q3', 'q4', 'q5'], \
-                     dtype={'year':int, 'uaid':int, 'patent_id':str, 'citation_id':str, 'q0':float, 'q1':float, 'q2':float, 'q3':float, 'q4':float, 'q5':float})
+                     dtype={'year':int, 'uaid':int, 'patent_id':str, 'citation_id':str, 'q0':float, 'q1':float, 'q2':float, 'q3':float, 'q4':float, 'q5':float}, \
+                         index_col=['year', 'uaid'])
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed Pre-processing")
-year1980 = df[df['year'] == 1980]
-retval1980 = year1980.to_csv('1980.csv')
+df.to_parquet(ut.citationFlowsParquet, compression='gzip')
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed Writing to Parquet")
 
 """
+year1980 = df[df['year'] == 1980]
+retval1980 = year1980.to_csv('1980.csv')
 
 #Pull entries by originating patent
 onepat = df[df['patent_id'] == "7364810"]
