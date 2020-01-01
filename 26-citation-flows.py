8b8a04b6a9f80d72f25167c3b198718b06059ecf
Ashwin <aiyenggar@users.noreply.github.com>
2020-01-01 19:01:32 +0530

    tried on small sample. parquet for the big one

---
+++ 26-citation-flows.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 19:38:36 2019
+
+@author: aiyenggar
+"""
+import citationutils as ut
+import csv
+import pandas as pd
+import time
+import geopy.distance
+
+flowsDict = {}
+uaDuplicatesDict = {}
+
+def getDistance(latlong1, latlong2):
+    retVal = ut.veryLargeValue
+    key1 = tuple([latlong1, latlong2])
+    key2 = tuple([latlong2, latlong1])
+    if key1 in distances_dict['distance']:
+        retVal = distances_dict['distance'][key1]
+    elif key2 in distances_dict['distance']:
+        retVal = distances_dict['distance'][key2]
+    else:
+        l_lat = latlong_dict['latitude'][latlong1]
+        l_long = latlong_dict['longitude'][latlong1]
+        r_lat = latlong_dict['latitude'][latlong2]
+        r_long = latlong_dict['longitude'][latlong2]
+        if (l_lat < r_lat + ut.degreeTreshold) and (l_lat > r_lat - ut.degreeTreshold) and (l_long < r_long + ut.degreeTreshold) and (l_long > r_long - ut.degreeTreshold):
+            retVal = round(geopy.distance.geodesic((l_lat, l_long),(r_lat, r_long)).km,2)
+            distances_dict['distance'][key1] = retVal
+    return retVal
+
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Beginning Pre-processing")
+summary_dict = pd.read_csv(ut.summaryFile, usecols = ['patent_id','cited_type1','cited_type2','cited_type3','cited_type4','cited_type5','precutoff_patents_cited','all_patents_cited','cnt_assignee','cnt_inventor'], dtype={'patent_id':str,'cited_type1':int,'cited_type2':int,'cited_type3':int,'cited_type4':int,'cited_type5':int,'precutoff_patents_cited':int,'all_patents_cited':int,'cnt_assignee':int,'cnt_inventor':int}, index_col='patent_id').to_dict()
+
+inv_uaid_dict = pd.read_csv(ut.keysFile1, usecols = ['patent_id','ualist'], dtype={'patent_id':str,'ualist':str}, index_col='patent_id').to_dict()
+assignee_dict = pd.read_csv(ut.keysFile1, usecols = ['patent_id','assigneelist'], dtype={'patent_id':str,'assigneelist':str}, index_col='patent_id').to_dict()
+inv_latlongid_dict = pd.read_csv(ut.keysFile1, usecols = ['patent_id','latlonglist'], dtype={'patent_id':str,'latlonglist':str}, index_col='patent_id').to_dict()
+
+if ut.calculateCitationDistance:
+    dist_df = pd.read_csv(ut.distancesFile, usecols = ['l_latlongid','r_latlongid','distance'], dtype={'l_latlongid':int,'r_latlongid':int,'distance':float})
+    dist_df.sort_values(by=['l_latlongid','r_latlongid'], inplace=True)
+    dist_df['lr']=list(zip(dist_df.l_latlongid, dist_df.r_latlongid))
+    dist_df.drop(columns=['l_latlongid','r_latlongid'], inplace=True)
+    dist_df.set_index('lr', inplace=True)
+    distances_dict = dist_df.to_dict()
+    latlong_dict = pd.read_csv(ut.latlongFile, usecols = ['latlongid','latitude','longitude'], dtype={'latlongid':int,'latitude':float,'longitude':float}, index_col='latlongid').to_dict()
+else:
+    distances_dict = None
+    latlong_dict = None
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed Pre-processing")
+
+distf = open(ut.citationDistanceUsedFileName, 'w', encoding='utf-8')
+distwriter = csv.writer(distf)
+distwriter.writerow(["category", "p_uaid", "c_uaid", "p_llid", "c_llid", "distance"])
+
+searchf = open(ut.searchFileName, 'r', encoding='utf-8')
+sreader = csv.reader(searchf)
+last_time = 0
+
+for citation in sreader:
+    if sreader.line_num == 1:
+        continue # we skip the header line
+
+    if sreader.line_num >= last_time + ut.update_freq_lines:
+        print(time.strftime("%Y-%m-%d %H:%M:%S") + " Processed " + str(sreader.line_num) + " raw citations")
+        last_time = sreader.line_num
+        distf.flush()
+
+    try:
+        year = int(citation[0])
+    except ValueError:
+        continue
+    patent_id = citation[1].strip() # to remove leading and trailing spaces
+    citation_id = citation[2].strip() # to remove leading and trailing spaces
+    if (len(patent_id) == 0) or (len(citation_id) == 0):
+        continue
+    precutoff_flag = False
+    try:
+        intcitation_id = int(citation_id)
+        if intcitation_id < 3930271:
+            precutoff_flag = True
+    except ValueError:
+        continue
+    type_citation = int(citation[3]) # 1 Null, 2 Applicant, 3 Examiner, 4 Other, 5 Third Party
+    seq_citation = int(citation[4])
+    kind_citation = citation[5] # B1, A etc
+
+    p_latlongid = ut.splitFromDict(patent_id, "latlonglist", ",", inv_latlongid_dict)
+    p_loc = ut.splitFromDict(patent_id, "ualist", ",", inv_uaid_dict)
+    p_ass = ut.splitFromDict(patent_id, "assigneelist", ",", assignee_dict)
+
+    c_latlongid = ut.splitFromDict(citation_id, "latlonglist", ",", inv_latlongid_dict)
+    c_loc = ut.splitFromDict(citation_id, "ualist", ",", inv_uaid_dict)
+    c_ass = ut.splitFromDict(citation_id, "assigneelist", ",", assignee_dict)
+
+    p_count_patents_cited = ut.readDict(patent_id, 'cited_type1', summary_dict) +\
+                            ut.readDict(patent_id, 'cited_type2', summary_dict) +\
+                            ut.readDict(patent_id, 'cited_type3', summary_dict) +\
+                            ut.readDict(patent_id, 'cited_type4', summary_dict) +\
+                            ut.readDict(patent_id, 'cited_type5', summary_dict)
+    p_count_assignees = ut.readDict(patent_id, 'cnt_assignee', summary_dict)
+    c_count_assignees = ut.readDict(citation_id, 'cnt_assignee', summary_dict)
+    c_count_inventors = ut.readDict(citation_id, 'cnt_inventor', summary_dict)
+    
+    for pind in range(len(p_loc)):
+        pflow = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        divisor = 1
+        if p_count_patents_cited > 1:
+            divisor *= p_count_patents_cited
+        if p_count_assignees > 1:
+            divisor *= p_count_assignees
+        if c_count_assignees > 1:
+            divisor *= c_count_assignees
+        if c_count_inventors > 1:
+            divisor *= c_count_inventors
+        flow_value = 1/divisor
+        patllid = int(p_latlongid[pind])
+        patloc = int(p_loc[pind])
+        if not ut.isValidUrbanArea(patloc):
+            continue
+        k1 = tuple([patloc, patent_id, citation_id])
+        if k1 in flowsDict:
+            if k1 in uaDuplicatesDict:
+                uaDuplicatesDict[k1] = uaDuplicatesDict[k1] + 1
+            else:
+                print(str(k1) + " No entry in duplicates list")
+            continue # We just increment duplicates, and avoid processing
+        for cind in range(len(c_loc)):
+            citllid = int(c_latlongid[cind])
+            citloc = int(c_loc[cind])
+            for apind in range(len(p_ass)):
+                patass = int(p_ass[apind])
+                for acind in range(len(c_ass)):
+                    citass = int(c_ass[acind])
+
+                    if ut.calculateCitationDistance and ut.isValidLatLongId(patllid) and ut.isValidLatLongId(citllid):
+                        if ut.isValidUrbanArea(patloc) and (not ut.isValidUrbanArea(citloc)):
+                            dt = getDistance(patllid, citllid)
+                            if (dt < ut.distanceTreshold):
+                                distwriter.writerow(['C', patloc, citloc, patllid, citllid, dt])
+                                citloc = patloc
+                        if ut.isValidUrbanArea(citloc) and (not ut.isValidUrbanArea(patloc)):
+                            dt = getDistance(patllid, citllid)
+                            if (dt < ut.distanceTreshold):
+                                distwriter.writerow(['P', patloc, citloc, patllid, citllid, dt])
+                                patloc = citloc
+
+                    if precutoff_flag or (not ut.isValidAssignee(patass)) and (not ut.isValidAssignee(citass)):
+                        quadrant = 5
+                    else:
+                        if patloc == citloc: # same urban area
+                            if patass == citass: # same assignee
+                                quadrant = 1
+                            else: # different assignee
+                                quadrant = 2
+                        else: # different urban area
+                            if patass == citass: # same assignee
+                                quadrant = 3
+                            else: # different assignee
+                                quadrant = 4
+                    pflow[quadrant] += flow_value
+        roundedflow = [round(x,4) for x in pflow]
+        k1 = tuple([patloc, patent_id, citation_id])
+        if k1 not in flowsDict:
+            flowsDict[k1] = [year] + roundedflow[1:]
+            uaDuplicatesDict[k1] = 1
+        else:
+            print(str(k1) + " already in flowsDict. Error. ")
+
+for k2 in uaDuplicatesDict:
+    if k2 in flowsDict:
+        flowsDict[k2] = flowsDict[k2] + [uaDuplicatesDict[k2]]
+    else:
+        print(str(k2) + "in Duplicates but not in Flows")
+        
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Creating DataFrame")
+df = pd.DataFrame([[k[0], k[1], k[2], v[0], v[1], v[2], v[3], v[4], v[5], v[6]] for k,v in flowsDict.items()]).rename(columns={0:'uaid', 1:'patent_id', 2:'citation_id',3:'year', 4:'q1', 5:'q2', 6:'q3', 7:'q4', 8:'q5', 9:'duplicates'}).set_index(['uaid', 'year'])
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed Creating DataFrame")
+df.to_parquet(ut.flowsFile, compression='gzip')
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed Writing to Parquet")
+    
+searchf.close()
+distf.close()
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed reading through search file")
--- 27-export-flows-parquet.py
+++ 27-export-flows-parquet.py
@@ -12,7 +12,7 @@ import time
 
 
 if __name__ == "__main__":
-    client = Client(n_workers=1, threads_per_worker=2, processes=False, memory_limit='2GB')
+    client = Client(n_workers=1, threads_per_worker=2, processes=False, memory_limit='4GB')
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Beginning Indexing")
 df = dd.read_csv(ut.citationFlowsFile, \
                  usecols = ['year', 'uaid', 'patent_id', 'citation_id', 'q0', 'q1', 'q2', 'q3', 'q4', 'q5'], \
