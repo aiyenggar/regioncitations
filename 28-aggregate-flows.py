049b45d6ddc55a07d499c1af9de38a03c6947b43
Ashwin <aiyenggar@users.noreply.github.com>
2020-01-02 19:42:48 +0530

    csv files still being written to, but seems good

--- 28-aggregate-flows.py
+++ 28-aggregate-flows.py
@@ -10,19 +10,16 @@ import pandas as pd
 import time
 
 maxindex = 10
-last_index = 0
-bigdf = None
-while last_index < maxindex:
-    last_index += 1
-    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting " + ut.singleUaidFlowsFile % last_index)
-    df = pd.read_parquet(ut.singleUaidFlowsFile % last_index)
-    if bigdf == None:
-        bigdf = df
-    else:
-        bigdf.append(df)
-    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Completed " + ut.singleUaidFlowsFile % last_index)
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting Writing to" + ut.flowsFile)    
+ldf = []
+for last_index in range(1, maxindex + 1):
+    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Reading " + ut.singleUaidFlowsFile % last_index)
+    ldf += [pd.read_parquet(ut.singleUaidFlowsFile % last_index)]
+    print(time.strftime("%Y-%m-%d %H:%M:%S") + " Read " + ut.singleUaidFlowsFile % last_index)
+    
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Concatenating")     
+bigdf = pd.concat(ldf)
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting Writing to " + ut.flowsFile)    
 bigdf.to_parquet(ut.flowsFile, compression='gzip')
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting Writing to" + ut.flowsFileCsv)    
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting Writing to " + ut.flowsFileCsv)    
 bigdf.to_csv(ut.flowsFileCsv)
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " Complted Writing to" + ut.flowsFileCsv)    
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " Complted Writing to " + ut.flowsFileCsv)    
