6335df569ae2c0d74d9fd8af10491ba468dac60a
Ashwin <aiyenggar@users.noreply.github.com>
2020-01-04 17:40:09 +0530

    full run for uaid less than 50 seems ok.

--- 28b-export-flows.py
+++ 28b-export-flows.py
@@ -12,6 +12,7 @@ import time
 
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Started Reading " + ut.flowsFile)     
 bigdf = pd.read_parquet(ut.flowsFile)
+
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Starting Writing to " + ut.flowsFileCsv)    
 bigdf.to_csv(ut.flowsFileCsv, index=False)
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " Complted Writing to " + ut.flowsFileCsv)    
